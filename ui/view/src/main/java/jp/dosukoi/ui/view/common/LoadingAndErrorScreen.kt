4f1d2fa3294cb8ab0b0f2c90e2a5618e7af70429
NaokiHidaka <dosukoroid@gmail.com>
2021-08-02 01:43:07 +0900

    loadingのアニメーションをLottieに置き換え

--- ui/view/src/main/java/jp/dosukoi/ui/view/common/LoadingAndErrorScreen.kt
+++ ui/view/src/main/java/jp/dosukoi/ui/view/common/LoadingAndErrorScreen.kt
@@ -3,13 +3,17 @@ package jp.dosukoi.ui.view.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import jp.dosukoi.ui.view.R
 import jp.dosukoi.ui.viewmodel.common.LoadState
 
 @Composable
@@ -19,23 +23,21 @@ fun <T> LoadingAndErrorScreen(
     onRetryClick: () -> Unit
 ) {
     when (state) {
-        is LoadState.Loading -> LoadingCircularProgressBar()
+        is LoadState.Loading -> LottieLoadingAnimation()
         is LoadState.Loaded -> loadedContent.invoke(state.value)
         is LoadState.Error -> LoadErrorContent(onRetryClick)
     }
 }
 
 @Composable
-private fun LoadingCircularProgressBar() {
+fun LottieLoadingAnimation() {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
-        CircularProgressIndicator(
-            modifier = Modifier.align(Alignment.Center),
-            color = black
-        )
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.loading_animation))
+        LottieAnimation(composition = composition)
     }
 }
 
