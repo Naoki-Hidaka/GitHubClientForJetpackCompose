1340f26be73d245bd41ff862e77deee62135029e
NaokiHidaka <dosukoroid@gmail.com>
2021-08-18 22:32:49 +0900

    replace reusable annotation with singleton annotation

--- data/repository/src/main/java/jp/dosukoi/data/repository/myPage/ReposRepository.kt
+++ data/repository/src/main/java/jp/dosukoi/data/repository/myPage/ReposRepository.kt
@@ -2,13 +2,13 @@ package jp.dosukoi.data.repository.myPage
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import dagger.Reusable
 import jp.dosukoi.data.api.common.IApiType
 import jp.dosukoi.data.entity.myPage.Repository
 import jp.dosukoi.data.repository.common.asyncFetch
 import javax.inject.Inject
+import javax.inject.Singleton
 
-@Reusable
+@Singleton
 class ReposRepository @Inject constructor(
     private val api: IApiType
 ) {
