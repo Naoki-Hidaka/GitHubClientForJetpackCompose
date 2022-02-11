a2425f8fd276000e15e3616076ac1efa5cc9d49d
NaokiHidaka <dosukoroid@gmail.com>
2022-02-11 23:49:08 +0900

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.5.10"
-        
+        val kotlinVersion = "1.6.10"
+
         classpath("com.android.tools.build:gradle:7.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
