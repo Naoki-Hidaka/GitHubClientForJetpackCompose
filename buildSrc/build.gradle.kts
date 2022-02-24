149cdcbdfa06b1160bcf29d680d63a377fe328b5
NaokiHidaka <dosukoroid@gmail.com>
2022-02-25 01:45:27 +0900

    fix versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.0.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    implementation("com.android.tools.build:gradle:7.1.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 }
