def9b90050389a91a96aac26add9e2f603d1351e
NaokiHidaka <dosukoroid@gmail.com>
2022-02-25 01:59:38 +0900

    fix kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.6.10"
+        val kotlinVersion = "1.5.10"
 
         classpath("com.android.tools.build:gradle:7.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
