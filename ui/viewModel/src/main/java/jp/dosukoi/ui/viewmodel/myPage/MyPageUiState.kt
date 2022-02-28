69300599bda75711c422b4fac7eaa53b77008715
NaokiHidaka <dosukoroid@gmail.com>
2022-02-28 23:32:28 +0900

    fix my page ui events

--- ui/viewModel/src/main/java/jp/dosukoi/ui/viewmodel/myPage/MyPageUiState.kt
+++ ui/viewModel/src/main/java/jp/dosukoi/ui/viewmodel/myPage/MyPageUiState.kt
@@ -2,9 +2,15 @@ package jp.dosukoi.ui.viewmodel.myPage
 
 import jp.dosukoi.data.entity.myPage.Repository
 import jp.dosukoi.data.entity.myPage.UserStatus
+import jp.dosukoi.ui.viewmodel.common.LoadState
 
 data class MyPageUiState(
+    val screenState: LoadState<MyPageScreenState> = LoadState.Loading,
+    val isRefreshing: Boolean = false,
+    val errors: List<Throwable> = emptyList()
+)
+
+data class MyPageScreenState(
     val userStatus: UserStatus,
-    val repositoryList: List<Repository>,
-    val isRefreshing: Boolean
+    val repositoryList: List<Repository>
 )
